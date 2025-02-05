2109c4cd21adead8f1231cdc728f975ecc5f23a2
Ritesh Jaswal <ritesh.jaswal@testingxperts.com>
2025-02-05 18:27:28 +0530

    Added Config Server

---
+++ ConfigServer/src/test/java/com/config_server/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config_server.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
